16bed781ef561c6abc1a827c78e11b09cce27c33
daichi0918 <kayasugadaichi@icloud.com>
2025-01-13 14:32:20 +0900

    localStorageのエラー解消

--- src/hooks/use_state_with_storage.tsx
+++ src/hooks/use_state_with_storage.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
 
 export const useStateWithStorage = (
   init: string,
   key: string
 ): [string, (s: string) => void] => {
-  const [value, setValue] = useState<string>(localStorage.getItem(key) || init);
+  const [value, setValue] = useState<string>(init); // 初期値は直接 `init` を使う
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue !== null) {
+      setValue(storedValue); // クライアントサイドで `localStorage` から値を取得
+    }
+  }, [key]);
 
   const setValueWithStorage = (nextValue: string): void => {
     setValue(nextValue);
